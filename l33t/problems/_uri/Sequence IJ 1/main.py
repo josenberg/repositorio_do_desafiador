cf3f7a25b4564b3079a899fac2f5155daba34ab0
Philipe Godoy <philipe.godoy@dextra-sw.com>
2020-05-21 21:55:37 -0300

    Sequence IJ 1

---
+++ l33t/problems/_uri/Sequence IJ 1/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Carry On Wayward Son there will be peace when you are done
+
+
+i = 1;
+for j in range(60, -5, -5):
+    print("I=" + str(i) + " J=" + str(j))
+    i = i + 3
